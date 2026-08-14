7711490cb55ff753bdb0a60450489cb622714418
amitprasad618 <amitprasad618@gmail.com>
2026-08-14 18:06:48 +0530

    Final first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import TextEditor from './components/TextEditor'
 export default function App(){
   return (
     <div className="app-root">
-      <header className="app-header">Support Communication Optimizer</header>
+      <header className="app-header">Support Communication Optimizer 
+        <span className="developer-tag">developed by Amit Prasad</span>
+      </header>
       <main>
         <TextEditor />
       </main>
